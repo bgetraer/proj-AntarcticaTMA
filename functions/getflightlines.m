d2621776159d7fafeb288519a897ac5e96c85e4a
Benjamin Getraer <benjamingetraer@gmail.com>
2025-05-06 14:21:05 -0400

    correct file name in relative path

--- functions/getflightlines.m
+++ functions/getflightlines.m
@@ -1,10 +1,10 @@
 function T = getflightlines(tma_num,datadir)
-   fname = fullfile(datadir,'/external/tma/index/USGS_TMA_Index_Flightlines/USGS_TMA_Index_Flightlines.dbf');
+   fname = fullfile(datadir,'/external/tma/index/USGS_TMA_Index_Flightlines/USGS_TMA_Index_flightlines.dbf');
    if ~exist(fname)
       url = 'https://data.pgc.umn.edu/aerial/usgs/tma/index/USGS_TMA_Index_Flightlines.zip';
       folder = fullfile(datadir,'external/tma/index/USGS_TMA_Index_Flightlines');
       unzip(url,folder)
-   end
+	end
    [data, names] = dbfread(fname);
    T = cell2table(data,'VariableNames',names);
    T.tma_num=str2double(T{:,2});
@@ -14,4 +14,7 @@ function T = getflightlines(tma_num,datadir)
    % extract just the flight lines we care about
    names = {'tma_num','acq_date','photoStart','photoEnd','length_km','altitudeFT','high_url'};
    T = sortrows(T(ismember(T.tma_num,tma_num),names),'acq_date');
+	% dont return duplicates
+	[~,i]=unique(T.tma_num);
+	T = T(i,:);
 end
